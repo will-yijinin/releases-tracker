650ed24d432f928f2e9f0b242c45627d8be4b535
sh07017ml <sh07017ml@sh07017mldeMacBook-Pro.local>
2022-02-17 18:08:22 +0800

    improve

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const http = require("http");
 const rss = require("./rss.ts");
 const { larkUrl, feedUrl } = require("./constants");
 const { default: list } = require("./list");
+const { Client } = require('pg');
 
 // route：新增larkUrl和feedUrl对。POST，params：larkUrl，feedUrl
 /**
@@ -16,7 +17,19 @@ const { default: list } = require("./list");
 // pm2 设置lifecycle management和自动重启。
 const app = http.createServer(async (request, response) => {
   // 每隔10分钟循环从数据库中获取列表
-  
+  const client = new Client({
+    connectionString: "postgres://zagqvwlvhksruv:f42ac867b90883c857ac488ab07839cabe90ab4fa458fcd15d5f79debf365138@ec2-54-155-194-191.eu-west-1.compute.amazonaws.com:5432/d5n63h01h3rse1",
+    ssl: true
+  })
+
+  async function main() {
+    await client.connect()
+    const res = await client.query('SELECT $1::text as message', ['Hello world!'])
+    console.log(res.rows[0].message) // Hello world!
+    await client.end()
+  }
+
+  main();
 
   // 根据列表，http请求获取最新feeds
   console.log("hello");
