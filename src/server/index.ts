fbe9ba7560e777c682b7fd629cec5a729afdef51
sh07017ml <sh07017ml@sh07017mldeMacBook-Pro.local>
2022-02-23 18:17:53 +0800

    add todo

--- src/server/index.ts
+++ src/server/index.ts
@@ -4,6 +4,12 @@ const express = require("express");
 const { main, addFeed, deleteFeed, updateOpUrl } = require("./routes");
 const cron = require('node-cron');
 
+// TODO: ts语法，interface，变量类型
+// TODO: ui: material ui
+// TODO: next.js
+// TODO: auth中间件
+// TODO: levelDB
+// TODO: 竞品的公告网站
 const app = express();
 const port = process.env.PORT || 8000;
 
@@ -25,6 +31,7 @@ db.init().then(() => {
 	// running a task every: dev - 1 minute, prod - 10 minutes
 	const timeRange = process.env.NODE_ENV==="development" ? "1" : "10";
 
+	// TODO: job单独一个app，和server分离
 	cron.schedule(`*/${timeRange} * * * *`, async () => {
 		// 生产: 每隔10分钟循环从数据库中获取列表
 		await main();
