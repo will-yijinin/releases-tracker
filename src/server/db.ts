cfabeb2a660833a3218729d03224ecff132e5c67
sh07017ml <sh07017ml@sh07017mldeMacBook-Pro.local>
2022-03-02 14:42:47 +0800

    commit

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('local');
+const db = new sqlite3.Database('localdb');
 
-const tableName = "local";
+const tableName = "localdb";
 
 /**
  * 数据库表结构：
